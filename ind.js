73a1d50cc065b8c3279b9a8fff4d6adc37ea78d7
Ajayi <JOSHUA BLESSING@DESKTOP-73KFCI4.com>
2024-08-01 23:49:25 +0100

    new changes

--- ind.js
+++ ind.js
@@ -70,3 +70,17 @@ function enter(){
 
 }
 
+
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = 
+  minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
